99bef6b020eaa6f670def970fb2103ea76fa5bc3
EL BIYAALI CHAIMAE p2211263 <chaimae.el-biyaali@etu.univ-lyon1.fr>
2023-01-27 11:17:16 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "openstack_networking_network_v2" "network-asbd-squeeze" {
 }
 
 resource "openstack_networking_subnet_v2" "asbd-subnet-front" {
-  network_id = "${openstack_networking_network_v2.network_1.id}"
+  network_id = "${openstack_networking_network_v2.network-asbd-squeeze.id}"
   name       = "asbd-subnet-front"
   cidr       = "10.245.199.0/24"
   dns_nameservers = ["10.10.10.10" ,"10.10.10.11"]

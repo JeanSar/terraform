8041f417c882730032d746507477d3715b945e3f
EL BIYAALI CHAIMAE p2211263 <chaimae.el-biyaali@etu.univ-lyon1.fr>
2023-01-27 10:32:51 +0100

    2 Création des fichiers de base

---
+++ provider.tf
@@ -0,0 +1,5 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+  auth_url    = "https://cloud-info.univ-lyon1.fr:5000/v3"
+  application_credential_id = "b39f43940cd24ca3b52b486de16a535e"
+}

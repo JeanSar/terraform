41513ed519b3166866eb60064ced577dac929100
EL BIYAALI CHAIMAE p2211263 <chaimae.el-biyaali@etu.univ-lyon1.fr>
2023-01-27 11:23:29 +0100

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 # Configure the OpenStack Provider
 provider "openstack" {
   auth_url    = "https://cloud-info.univ-lyon1.fr:5000/v3"
-  application_credential_id = "b39f43940cd24ca3b52b486de16a535e"
+  application_credential_id = "9775ddba335b45799dca1ae605909631"
 }
